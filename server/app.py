51817e0370ff7096bfc70bf685078e367c76794d
marilyn <marilyn.oyoo@student.moringaschool.com>
2024-11-23 21:08:33 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+# append to server/app.py
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
+
+
